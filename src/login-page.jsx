f81f39feb6abbd9dd233cb8ae2b7c97fc31932fd
Kim Duclos <kimduclos@gmail.com>
2021-07-22 17:53:19 -0700

    login-page files created

---
+++ src/login-page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <div className="blue-left">test</div>
+      <div className="gray-right">test</div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/logo.jsx
@@ -0,0 +1,13 @@
+import { logRoles } from "@testing-library/react";
+import React from "react";
+import "./logo.scss";
+
+const Logo = () => {
+    return (
+       <div className="logo">
+           Pace Partner
+       </div>
+    );
+}
+
+export default Logo;
